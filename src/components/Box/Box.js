08db033c3940dc9d17a46b008f5aa94ea5c1eff4
vsrandom <viveksharma2498@gmail.com>
2020-09-21 17:43:45 +0530

    Bugs fixed and animation added

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -2,8 +2,13 @@ import React from 'react'
 import classes from './Box.module.css'
 
 const box = (props)=>{
+    const cs = [classes.item];
+    if(props.hlt)
+    {
+        cs.push(classes.highlight)
+    }
     return (
-        <div className = {classes.item} onClick = {props.click}>
+        <div className = {cs.join(' ')} onClick = {props.click}>
             <div>
                 {props.value}
             </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,13 @@
 import React, {Component} from 'react';
-import logo from '../logo.svg';
+// import logo from '../logo.svg';
 import classes from './App.module.css'
 import Box from '../components/Box/Box'
-
-let GLOBAL_STATE = {
-  Game : [
-    [null,null,null],
-    [null,null,null],
-    [null,null,null]
-  ],
-  Turn : 'X',
-  Winner : "None"
-}
 class App extends Component{
     state = {
+      winningBoxes : [
+        null,null,null,null,null,
+        null,null,null,null
+      ],
       gameArray : [
         [null,null,null],
         [null,null,null],
@@ -24,18 +18,24 @@ class App extends Component{
     }
 
     checkWinnerHandler = (x,y,mark)=>{
-        let tx = x;
-        let ty = y;
         // check horizontal , vertical , diagonal
-        let gameArrayTemp = this.state.gameArray;
-        let h = 0, v = 0, d=0;
+        let gameArrayTemp = [...this.state.gameArray];
+        let winningBoxesTemp = [...this.state.winningBoxes];
+        let h = 0, v = 0, d=0, dr=0;
         let j=0; 
         for(;j<=2;j++)
         {
           if(gameArrayTemp[x][j] !== mark)
           break;
         }
-        if(j===3) h=1;
+        if(j===3)
+        {
+          h=1;
+          for(let j=0;j<=2;j++)
+          {
+            winningBoxesTemp[3*x+j] = 1;
+          }
+        } 
 
         j=0;
 
@@ -45,7 +45,14 @@ class App extends Component{
           break;
         }
 
-        if(j===3) h=1;
+        if(j===3)
+        {
+          h=1;
+          for(let j=0;j<=2;j++)
+          {
+            winningBoxesTemp[y+3*j] = 1;
+          }
+        } 
           
         j=0;
 
@@ -55,51 +62,93 @@ class App extends Component{
           break;
         }
 
-        if(j===3) d =1;
-
-        if(h===1 || v===1 || d===1) 
+        if(j===3)
         {
-          this.setState({winner : mark});
+          d = 1;
+          winningBoxesTemp[0] = 1;
+          winningBoxesTemp[4] = 1;
+          winningBoxesTemp[8] = 1;
+        }
+
+        j=2;
+        let ridx = 0;
+        for(;j>=0;j--)
+        {
+          if(gameArrayTemp[ridx][j]!== mark)
+          break;
+          ridx++;
+        }
+
+        if(j==-1)
+        {
+          dr = 1;
+          winningBoxesTemp[2] = 1;
+          winningBoxesTemp[4] = 1;
+          winningBoxesTemp[6] = 1; 
+        }
+
+
+        if(h===1 || v===1 || d===1 || dr===1) 
+        {
+          this.setState({winner : mark, winningBoxes : winningBoxesTemp});
         }
     }
 
     onClickHandler = (x,y)=>{
-        let gameArrayTemp = [...this.state.gameArray];
-        let currentTurnTemp = this.state.currentTurn;
-        gameArrayTemp[x][y] = currentTurnTemp;
-        if(currentTurnTemp === 'X') currentTurnTemp = 'O'
-        else currentTurnTemp = 'X';
-        this.setState({gameArray : gameArrayTemp, currentTurn : currentTurnTemp})
-        this.checkWinnerHandler(x,y,gameArrayTemp[x][y]);
+        if(this.state.winner === "None"){
+          if(this.state.gameArray[x][y] === null){
+            let gameArrayTemp = [...this.state.gameArray];
+            let currentTurnTemp = this.state.currentTurn;
+            gameArrayTemp[x][y] = currentTurnTemp;
+            if(currentTurnTemp === 'X') currentTurnTemp = 'O'
+            else currentTurnTemp = 'X';
+            this.setState({gameArray : gameArrayTemp, currentTurn : currentTurnTemp})
+            this.checkWinnerHandler(x,y,gameArrayTemp[x][y]);
+          }
+        }
     }
 
     resetHandler = ()=>{
-        let gameArrayTemp = this.state.gameArray;
+        let gameArrayTemp = [...this.state.gameArray];
         for(let i=0;i<=2;i++)
         {
           for(let j=0;j<=2;j++) gameArrayTemp[i][j] = null;
         }
-
-        this.setState({gameArray : gameArrayTemp, winner : "None", currentTurn : 'X'})
+        let winningBoxesTemp = [...this.state.winningBoxes];
+        for(let i = 0;i<9;i++) winningBoxesTemp[i] = null;
+        this.setState({gameArray : gameArrayTemp, winner : "None", currentTurn : 'X',winningBoxes : winningBoxesTemp})
     }
 
     render(){
+      const csb = [classes.button]
+
+      const csw = [classes.winner]
+
+      if(this.state.winner != "None")
+      {
+        csb.push(classes.highlightButton);
+        csw.push(classes.winnerHighlight);
+      }
+
       return(
         <div className = {classes.container1}>
-          <div className = {classes.winner}> <div>Winner : {this.state.winner}</div></div>
+          <div className = {classes.heading}><h1>TIC TAC TOE</h1></div>
+          <div className = {classes.winnerContainer}>
+            <div className = {csw.join(' ')}> <div>Winner : {this.state.winner}</div></div>
+            <div className = {csb.join(' ')} onClick = {this.resetHandler}><div>RESET</div></div>
+          </div>
           <div className = {classes.turn}><div>{this.state.currentTurn} : Player's Turn</div></div>
           <div className = {classes.container2}>
-              <Box  value = {this.state.gameArray[0][0]} click = {()=>this.onClickHandler(0,0)}/>
-              <Box  value = {this.state.gameArray[0][1]} click = {()=>this.onClickHandler(0,1)}/>
-              <Box  value = {this.state.gameArray[0][2]} click = {()=>this.onClickHandler(0,2)}/>
-              <Box  value = {this.state.gameArray[1][0]} click = {()=>this.onClickHandler(1,0)}/>
-              <Box  value = {this.state.gameArray[1][1]} click = {()=>this.onClickHandler(1,1)}/>
-              <Box  value = {this.state.gameArray[1][2]} click = {()=>this.onClickHandler(1,2)}/>
-              <Box  value = {this.state.gameArray[2][0]} click = {()=>this.onClickHandler(2,0)}/>
-              <Box  value = {this.state.gameArray[2][1]} click = {()=>this.onClickHandler(2,1)}/>
-              <Box  value = {this.state.gameArray[2][2]} click = {()=>this.onClickHandler(2,2)}/>                     
-          </div>
-          <div className = {classes.button}><div onClick = {this.resetHandler}>RESET</div></div>
+              <Box  hlt = {this.state.winningBoxes[0]} value = {this.state.gameArray[0][0]} click = {()=>this.onClickHandler(0,0)}/>
+              <Box  hlt = {this.state.winningBoxes[1]}  value = {this.state.gameArray[0][1]} click = {()=>this.onClickHandler(0,1)}/>
+              <Box  hlt = {this.state.winningBoxes[2]}  value = {this.state.gameArray[0][2]} click = {()=>this.onClickHandler(0,2)}/>
+              <Box  hlt = {this.state.winningBoxes[3]}  value = {this.state.gameArray[1][0]} click = {()=>this.onClickHandler(1,0)}/>
+              <Box  hlt = {this.state.winningBoxes[4]}  value = {this.state.gameArray[1][1]} click = {()=>this.onClickHandler(1,1)}/>
+              <Box  hlt = {this.state.winningBoxes[5]}  value = {this.state.gameArray[1][2]} click = {()=>this.onClickHandler(1,2)}/>
+              <Box  hlt = {this.state.winningBoxes[6]}  value = {this.state.gameArray[2][0]} click = {()=>this.onClickHandler(2,0)}/>
+              <Box  hlt = {this.state.winningBoxes[7]}  value = {this.state.gameArray[2][1]} click = {()=>this.onClickHandler(2,1)}/>
+              <Box  hlt = {this.state.winningBoxes[8]}  value = {this.state.gameArray[2][2]} click = {()=>this.onClickHandler(2,2)}/>                     
+          </div>  
         </div>
       )
     }
